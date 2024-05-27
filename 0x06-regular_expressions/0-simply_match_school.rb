029326def7930f7c069e7ba5c161045ddce6439c
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-05-27 20:10:00 +0200

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+if ARGV.length != 1
+  puts "Usage: #{$0} <string_to_search>"
+  exit 1
+end
+
+search_term = "School"
+occurrences = ARGV[0].scan(search_term)
+
+if occurrences.empty?
+  puts "No occurrences of '#{search_term}' found in the given input."
+else
+  puts "Occurrences of '#{search_term}': #{occurrences.size}"
+end
