f2902c73eb0a03f3d9cbbb08e525eeea89c5353e
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-05-27 20:24:35 +0200

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -5,7 +5,7 @@ if ARGV.length != 1
   exit 1
 end
 
-if ARGV[0].match?(/^[0-9]{10}$/)
+if ARGV[0].match?(/^[0-9]{1,10}$/)
   puts ARGV[0]
 else
   puts "Invalid input. Please provide a 10-digit number."
