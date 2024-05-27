01d120ebced51a4d406a22297ca94918705f2f76
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-05-27 20:13:40 +0200

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join
+
+if ARGV.length != 1
+  puts "Usage: #{$0} <string_to_search>"
+  exit 1
+end
+
+search_pattern = /h[a-z]{2,5}n/
+matches = ARGV[0].scan(search_pattern)
+
+if matches.empty?
+  puts "No matches found."
+else
+  puts matches.join(" ")
+end
